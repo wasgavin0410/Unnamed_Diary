8ca5ba16f9ef71ada5084ad9238cce4ed55cb13a
Gavin_Kamikase <wasgavin0410@gmail.com>
2018-05-20 01:13:53 +0800

    Not sure about what's im doing
    k, keep trying to resolve the conflicts happened at merging.

--- app/src/main/java/com/example/gavin/unnamed_diary/main_diaryPage.java
+++ app/src/main/java/com/example/gavin/unnamed_diary/main_diaryPage.java
@@ -1,6 +1,8 @@
 package com.example.gavin.unnamed_diary;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
